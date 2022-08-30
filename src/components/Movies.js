950bba389d2dfd8311091995704f1127ab37e501
stephenChinag <stephenchinag@gmail.com>
2022-08-30 09:28:41 +0100

    first commit

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,14 +2,15 @@ import React from "react";
 import styles from './Movies.module.css'
 
 
-const Movie=(props)=>{
+const Movies=(props)=>{
     return(
         <li className={styles.movie}>
             <h2>{props.title}</h2>
+            <p>{props.openingText}</p>
             <h3>{props.releaseDate}</h3>
-            <p>{props.openiningText}</p>
+           
 
         </li>
     )
 }
-export default Movie;
+export default Movies;
---
+++ src/components/MoviesList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Movies from "./Movies";
+import styles from './MoviesList.module.css'
+
+
+const MoviesList =(props)=>{
+    return (
+        <ul className={styles[`movies-list`]}>
+
+            {props.movies.map((movie) =>(
+                <Movies 
+                key={movie.id}
+                title ={movie.title}
+                openingText={movie.openingText}
+                releaseDate={movie.releaseDate}
+                />
+            ))}
+        </ul>
+    )
+}
+
+export default  MoviesList;
